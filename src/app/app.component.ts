ca7e6f4d9a882839b5b9d790962160155e37f9b0
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2025-01-19 12:24:33 +0200

    set up firebase hosting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: false,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+
   title = 'baatbg-cms';
+
+
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAWmDho7AeyXNJXgY1tsu6twXh9sPg27qA",
+        authDomain: "baatbgorg.firebaseapp.com",
+        databaseURL: "https://baatbgorg-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "baatbgorg",
+        storageBucket: "baatbgorg.firebasestorage.app",
+        messagingSenderId: "173955335103",
+        appId: "1:173955335103:web:2043ff36eaa0502236970a",
+        measurementId: "G-L97SQVHZ2X"
+    }
+};
